e90eba0b3d3b4106885003a5103770c1c3774434
yapie <targaryen.akane@gmail.com>
2019-08-30 16:27:39 +0700

    commit

--- main.js
+++ main.js
@@ -13,28 +13,15 @@ function watchStatus() {
   gitStatus.stdout.on('data', data => {
     console.log(`${data}`);
   });
-
-  gitStatus.stderr.on('data', data => {
-    errHandler(data);
-  });
-
-  // gitStatus.on('close', code => {
-  // console.log(`child process exited with code ${code}`);
-  // });
 }
 
-function clear() {
+function main() {
   exec('clear', (error, stdout, stderr) => {
     if (error) {
       errHandler(error);
     }
     console.log(`${stdout}`);
-    // console.error(`stderr: ${stderr}`);
+    watchStatus();
   });
-}
-
-function main() {
-  clear();
-  watchStatus();
 }
 setInterval(main, 1000);
